e95bb6ccc44f547f4bd09c140b723236c69e12ea
Coder-Matheo <ali.dinarvand1370@gmail.com>
2021-09-10 01:07:17 +0200

    navigation drawer added

---
+++ app/src/main/java/rob/myappcompany/navigationdrawerdemo/Pro_ViewHolder.java
@@ -0,0 +1,21 @@
+package rob.myappcompany.ticket_system_;
+
+import android.view.View;
+import android.widget.TextView;
+
+
+
+public class Pro_ViewHolder {
+    //hold TextView from UI to Adapter
+    TextView createrTextView;
+    TextView quastionTextView;
+    TextView createdTextView;
+
+    Pro_ViewHolder(View view){
+        createrTextView = view.findViewById(R.id.createrTextView);
+        quastionTextView = view.findViewById(R.id.questionTextView);
+        createdTextView = view.findViewById(R.id.createdDateTextView);
+    }
+
+
+}
---
+++ app/src/main/java/rob/myappcompany/navigationdrawerdemo/TicketAdapter.java
@@ -0,0 +1,57 @@
+package rob.myappcompany.ticket_system_;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter<String> {
+
+    ArrayList<String> createrList;
+    ArrayList<String> questionList;
+    ArrayList<String> ticketNameList;
+    ArrayList<String> titleCreatedList;
+    ArrayList<String> situationList;
+    ArrayList<String> priorityList;
+    ArrayList<String> createdDateList;
+    ArrayList<String> updatedDateList;
+
+
+    public CustomAdapter(Context context, ArrayList<String> createrListIn, ArrayList<String> quastionListIn, ArrayList<String>situationList) {
+        super(context, R.layout.single_item_custom_view, R.id.createrTextView, createrListIn);
+        this.createrList= createrListIn;
+        this.questionList=quastionListIn;
+        this.situationList=situationList;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View singleItemView = convertView;
+        Pro_ViewHolder holder = null;
+        if (singleItemView == null){
+            LayoutInflater layoutInflater =(LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            singleItemView = layoutInflater.inflate(R.layout.single_item_custom_view, parent, false);
+            holder = new Pro_ViewHolder(singleItemView);
+            singleItemView.setTag(holder);
+        }
+        else
+        {
+            holder = (Pro_ViewHolder) singleItemView.getTag();
+        }
+
+        holder.createrTextView.setText(createrList.get(position));
+        holder.quastionTextView.setText(questionList.get(position));
+        holder.createdTextView.setText(situationList.get(position));
+
+
+        return singleItemView;
+    }
+}
